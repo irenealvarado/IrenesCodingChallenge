b9a99f248152d6574d24ef0d3d3062ff487c3a8e
Irene <irenesalvarado29@gmail.com>
2021-07-08 09:39:08 -0400

    adding nanoid to generate unique keys for image list

--- client/components/ImageItem.js
+++ client/components/ImageItem.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { StyleSheet, View, Image, ScrollView } from 'react-native'
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
 import { Text } from 'react-native-elements'
+import { nanoid } from 'nanoid/non-secure'
 
 const styles = StyleSheet.create({
     container: {
@@ -16,17 +17,18 @@ const styles = StyleSheet.create({
 
 const ImageItem = (props) => {
     const { data } = props
+
     return (
         <ScrollView style={styles.container}>
             <Card>
-                {data.map((imageComment) => (
-                    <View>
+                {data.map(({ imagePath, imageDescription }) => (
+                    <View key={nanoid()}>
                         <Image
                             style={styles.image}
-                            source={data.map((images) => images.imagePath)}
+                            source={{ uri: imagePath }}
                         />
                         <Text h5 style={{ padding: 5 }}>
-                            {imageComment.imageDescription}
+                            {imageDescription}
                         </Text>
                         <Card.Divider />
                     </View>
